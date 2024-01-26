0fa72cbfdc2e9e75a2d68e84600b23c384b5a4e0
Anna <124448052+LeonovaA1987@users.noreply.github.com>
2024-01-26 20:47:57 +0600

    Create calc.py
    Класс для калькулятора

---
+++ calc.py
@@ -0,0 +1,17 @@
+# from app.calculator.py import Calculator
+class Calculator:
+   def multiply(self, x, y):
+       return x * y
+
+   def division(self, x, y):
+       return x / y
+
+   def subtraction(self, x, y):
+       return x - y
+
+   def adding(self, x, y):
+       return x + y
+
+
+
+
